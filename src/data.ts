90a5a8c3ff10a1ad25852d460745af9f54a4b9d2
munyua <mrmunyuajm@gmail.com>
2026-08-11 16:18:37 +0300

    Add default tasks file

---
+++ src/data.ts
@@ -0,0 +1,7 @@
+export const defaultTasks = [
+  { id: 1, text: "Learn React basics", completed: true },
+  { id: 2, text: "Brush up Typescript fundamentals", completed: true },
+  { id: 3, text: "Build a todo app", completed: true },
+  { id: 4, text: "Add Tailwind styling", completed: false },
+  { id: 5, text: "Deploy to production", completed: false },
+];
