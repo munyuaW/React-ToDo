4fc665a4d2a06370fa48d497230ba15101a5adc6
munyua <mrmunyuajm@gmail.com>
2026-08-12 12:19:31 +0300

    Add the TodoItem component to the project. This component represents a single todo item with a checkbox and text.

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,23 @@
+// import type { TodoItemProps } from "../types";
+import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
+function TodoItem() {
+  return (
+    <div className="flex items-center gap-2.5 border px-2 py-4 border-slate-700 rounded-lg">
+      <input
+        type="checkbox"
+        className="size-5 border-2  rounded-full bg-transparent border-slate-700 text-slate-100 checked:bg-cyan-500 checked:border-cyan-500 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-transparent transition-colors cursor-pointer active:scale-95"
+      />
+      <span className="flex-1 line-through">
+        Brush up Typescript fundamentals
+      </span>
+      <button className="p-1 rounded-sm cursor-pointer -mr-2">
+        <FaRegEdit className="size-4 text-emerald-400" />
+      </button>
+      <button className="p-1 rounded-sm cursor-pointer -mr-2">
+        <FaTrashAlt className="size-4 text-orange-700" />
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
