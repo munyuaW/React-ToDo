9149b31f7ded99f698e9c579bc9a5b5a745ba0df
munyua <mrmunyuajm@gmail.com>
2026-08-12 13:37:42 +0300

    render the todoItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import Header from "./components/Header";
 import Stats from "./components/Stats";
+import TaskInputForm from "./components/TaskInputForm";
+import TodoItem from "./components/TodoItem";
+import { defaultTasks } from "./data";
 
 export default function App() {
+  function addNewTask(formData: FormData) {
+    const input = formData.get("task");
+    console.log(input);
+  }
+
+  function toggleChecked() {}
+  function editTask() {}
+  function deleteTask() {}
+
+  const todoItems = defaultTasks.map((todo) => (
+    <TodoItem
+      key={todo.id}
+      todo={todo}
+      onToggle={toggleChecked}
+      onEdit={editTask}
+      onDelete={deleteTask}
+    />
+  ));
+
   return (
     <div className="min-h-screen flex justify-center items-center px-3 py-6">
       {/* ToDo container as a card */}
-      <div className="relative w-full max-w-md bg-slate-900/30 backdrop-blur-xl border border-slate-800 p-8 rounded-3xl shadow-2xl">
+      <div className="relative w-full max-w-md bg-slate-900/30 backdrop-blur-xl border border-slate-800 px-4 py-8 rounded-3xl shadow-2xl">
         {/* The Glow */}
         <div className="absolute -top-20 left-1/2 -translate-x-1/2 w-64 h-64 bg-cyan-500/20 rounded-full blur-3xl"></div>
         <div>
           <Header />
           <Stats />
-          <div>Task Input</div>
-          <div>Tasks Container</div>
+          <TaskInputForm addTask={addNewTask} />
+          <div className="space-y-2">{todoItems}</div>
         </div>
       </div>
     </div>
