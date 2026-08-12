a204e21a562de7794352b88cf7e8aafb8da72f5e
munyua <mrmunyuajm@gmail.com>
2026-08-12 08:57:26 +0300

    Added TaskInputForm component to handle task input and submission.

---
+++ src/components/TaskInputForm.tsx
@@ -0,0 +1,21 @@
+function TaskInputForm(props) {
+  return (
+    <div className="mb-6">
+      <form action={props.addTask}>
+        <div className="flex gap-2 flex-wrap">
+          <input
+            type="text"
+            placeholder="Add a new task..."
+            name="task"
+            className="flex-1 bg-slate-800 px-4 py-3 rounded-lg border border-slate-700 focus:outline-none focus:border-cyan-500 text-slate-100 placeholder:text-slate-400"
+          />
+          <button className="bg-cyan-500 px-4 py-3 rounded-lg border-none font-semibold w-full sm:w-fit transition-colors hover:bg-cyan-600 cursor-pointer active:scale-95">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default TaskInputForm;
