0b4bb2b499343294c84b386000625154c8596493
munyua <mrmunyuajm@gmail.com>
2026-08-12 12:28:29 +0300

    change interface from TodoItem to TodoItemProps.

--- src/types.ts
+++ src/types.ts
@@ -4,8 +4,9 @@ export interface Todo {
   completed: boolean;
 }
 
-export interface TodoItem {
+export interface TodoItemProps {
   todo: Todo;
+  onToggle: (id: string) => void;
   onEdit: (id: string) => void;
   onDelete: (id: string) => void;
 }
