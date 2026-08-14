d379f88e00805e6c072cf010af719ecbfe36bf6d
munyua <mrmunyuajm@gmail.com>
2026-08-15 01:40:24 +0300

    Add more types.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { MouseEventHandler } from "react";
+
 export interface Todo {
   id: string;
   text: string;
@@ -15,4 +17,36 @@ export interface StatsProps {
   total: number;
   completed: number;
   percentage: number;
+}
+
+export interface ButtonProps {
+  text: string;
+  bgColor: string;
+  textColor?: string;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface ModalProps {
+  isEdit: boolean;
+  editIndex?: number;
+  editText?: string;
+  setEditText?: (text: string) => void;
+  onSave: () => void;
+  onConfirm: (id: string) => void;
+  deleteId?: string;
+  onCancel: () => void;
+}
+
+export interface EditBoxProps {
+  editIndex: number | undefined;
+  editText: string | undefined;
+  setEditText: (text: string | undefined) => void;
+  saveEdit: () => void;
+  cancelEdit: () => void;
+}
+
+export interface ConfirmDeleteBoxProps {
+  deleteId: string | undefined;
+  confirmDelete: (id: string) => void;
+  cancelDelete: () => void;
 }
